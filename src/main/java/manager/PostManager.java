db53730282d261a1354a83c497bb82378519c857
Evgenii Semenov <eo.semenoff@gmail.com>
2021-06-11 17:54:41 +0300

    TASK2. PostManager added

---
+++ src/main/java/manager/PostManager.java
@@ -0,0 +1,11 @@
+package manager;
+
+public class PostManager {
+    public PostService[] search(int ownerID, String domain, String query, boolean ownersOnly, int count, int offset) {
+        return null;
+    }
+
+    public void delete(int ownerId, int postId) {
+    }
+}
+
---
+++ src/main/java/manager/PostService.java
@@ -0,0 +1,36 @@
+package manager;
+
+import domain.*;
+
+public class PostService {
+    private int postId;
+    private int ownerId;
+    private int fromId;
+    private int createdBy;
+    private int date;
+    private String text;
+    private int replyOwnerId;
+    private int replyPostId;
+    private int friendsOnly;  // private boolean friendsOnly; ????
+    private CommentsInfo commentsInfo;
+    private Copyright copyright;
+    private LikesInfo likesInfo;
+    private RepostsInfo repostsInfo;
+    private ViewsInfo viewsInfo;
+    private String postType;
+    private PostSource postSource;
+    private Geo geo;
+    private int signerId;
+    private int canPin;
+    private int canDelete;
+    private int canEdit;
+    private int isPinned;
+    private int markAsAds;
+    private boolean isFavourite;
+    private int postopenedID;
+
+//++getters/setters
+}
+
+
+
