e00ea7481e1079182477d040bd22d6611f303077
Evgenii Semenov <eo.semenoff@gmail.com>
2021-06-11 16:05:34 +0300

    PostService and PostManager added

---
+++ src/main/java/domain/PostManager.java
@@ -0,0 +1,16 @@
+package domain;
+
+public class PostManager {
+    private boolean userIsAuthor;
+
+    {
+        // Логика, дающая права автору поста на его администрирование
+    }
+
+    private boolean deletePost;
+    private boolean modifyPost;
+    private boolean pinUpPost;
+    private boolean deleteComments;
+
+}
+
---
+++ src/main/java/domain/PostService.java
@@ -0,0 +1,20 @@
+package domain;
+
+public class PostService {
+    private int authorId;
+    private boolean userIsAuthor;
+    private String dateOfPost;
+    private String timeOfPost;
+    private boolean postIsPinned;
+    private boolean addAsBookmark;
+    private int likesCounter;
+    private String[] likersList;
+    private String[] comments;
+    private int viewsCounter;
+    private int repostCounter;
+
+//++getters/setters
+}
+
+
+
